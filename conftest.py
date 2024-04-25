e94c04d949550d7561a2130bc0bb17712efe44b1
Komp-880-PC <canek2018mail.ru@gmail.com>
2024-04-25 19:47:33 +0300

    Add file

---
+++ conftest.py
@@ -0,0 +1,18 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+def pytest_addoption(parser):
+    parser.addoption("--browser name", action="store", default="chrome", help="Choose browser: chrome")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser name")
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+    else:
+        raise pytest.UsageError("--browser name should be chrome")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
